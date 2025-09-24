f9841a90268722309091e15d2b397328535a95c5
Arseniy Merzlyakov <merzliakov.ad@phystech.edu>
2025-09-24 18:22:56 +0300

    get instr codes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
-int main()
+#include "decoder.hpp"
+#include <cassert>
+
+int main(int argc, char** argv)
 {
+    assert(argc == 2);
+
+    std::string filename(argv[1]);
+    auto instrs = decoder::get_instructions(filename);
+    decoder::dump_instrs(instrs);
     
+    return 0;
 }
